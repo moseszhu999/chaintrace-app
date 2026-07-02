5d780dee635185c4a1abf11a30dc9fe0b6a6dc01
moseszhu999 <138568031+moseszhu999@users.noreply.github.com>
2026-07-02 22:25:03 +0800

    Use P1 adapter dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,10 @@
-import { DashboardPage } from "@/components/p1-pages";
+import { AdapterDashboardPage } from "@/components/p1-adapter-pages";
 import { P1Shell } from "@/components/p1-shell";
 
 export default function Page() {
   return (
     <P1Shell>
-      <DashboardPage />
+      <AdapterDashboardPage />
     </P1Shell>
   );
 }
