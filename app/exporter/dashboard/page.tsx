77a8e6d89d86296ca3919f0b674d96d3f43241fa
moseszhu999 <138568031+moseszhu999@users.noreply.github.com>
2026-07-02 22:25:13 +0800

    Use P1 adapter exporter dashboard

--- app/exporter/dashboard/page.tsx
+++ app/exporter/dashboard/page.tsx
@@ -1,10 +1,10 @@
-import { ExporterDashboardPage } from "@/components/p1-pages";
+import { AdapterExporterDashboardPage } from "@/components/p1-adapter-pages";
 import { P1Shell } from "@/components/p1-shell";
 
 export default function Page() {
   return (
     <P1Shell requiredRole="EXPORTER">
-      <ExporterDashboardPage />
+      <AdapterExporterDashboardPage />
     </P1Shell>
   );
 }
